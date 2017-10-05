fc7d3e72e77f366bc1571e6b93438151e0bc2b5c
avohra <avohra@engagepeople.com>
2017-10-04 23:49:31 -0400

    copied over images, directory structure, added header, home and footer component, added basic styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppURLRepo } from '../../utils/app-url-repo';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public notificationManagerLogo: string;
+
+  constructor() {
+    this.notificationManagerLogo = AppURLRepo.NOTIFICATIONS_MANAGER_LOGO;
+  }
 
   ngOnInit() {
   }
---
+++ src/utils/app-url-repo.ts
@@ -0,0 +1,9 @@
+export class AppURLRepo {
+
+  public static readonly ASSETS_URL: string = 'assets';
+
+  public static readonly IMG_URL: string = AppURLRepo.ASSETS_URL + '/img';
+
+  public static readonly NOTIFICATIONS_MANAGER_LOGO: string = AppURLRepo.IMG_URL + '/' + 'logo-MN.png';
+
+}
