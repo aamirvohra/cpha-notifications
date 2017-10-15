2919a3d2f2d66ee49edc870fff90ecd387475fe0
avohra <avohra@engagepeople.com>
2017-10-14 23:15:33 -0400

    reports completed for tablet version

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { AppURLRepo } from '../../utils/app-url-repo';
 import { ModalService } from '../services/modal-service.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   public notificationManagerLogo: string;
 
@@ -15,13 +16,30 @@ export class HeaderComponent {
   public dropdownIcon: string;
   public settingIcon: string;
   public signoutIcon: string;
+  public loggedInUser: boolean;
 
-  constructor(private modalService: ModalService) {
+  constructor(private modalService: ModalService,
+              private router: Router) {
     this.notificationManagerLogo = AppURLRepo.NOTIFICATIONS_MANAGER_LOGO;
     this.profileIcon = AppURLRepo.PROFILE_ICON;
     this.dropdownIcon = AppURLRepo.DROPDOWN_ICON;
     this.settingIcon = AppURLRepo.SETTINGS_ICON;
     this.signoutIcon = AppURLRepo.SIGNOUT_ICON;
+  }
+
+  ngOnInit() {
+    this.router.events.filter(
+      event => event instanceof NavigationEnd).subscribe(
+      (event: NavigationEnd) => {
+        if (event.url !== '/') {
+          this.loggedInUser = true;
+        }
+        else {
+          this.loggedInUser = false;
+        }
+      }
+    )
+
   }
 
   displaySignInForm() {
