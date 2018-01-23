e11d9e76c2cb68a6fdd09d705423c240cf66659f
Aamir Vohra <aamir_vohra@hotmail.com>
2018-01-22 23:03:08 -0500

    moved modal opening logic to header component for login and register component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { ModalService } from './services/modal-service.service';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
@@ -13,80 +9,7 @@ import { RegisterComponent } from './register/register.component';
 export class AppComponent {
   title = 'app';
 
-  public signInModalRef: BsModalRef;
-  public registerModalRef: BsModalRef;
-
-  public modalOptions: any;
-
-  constructor(private modalService: ModalService, private modal: BsModalService) {
-    this.modalOptions = {
-      ignoreBackdropClick: true,
-    };
-
-    this.modalService._displaySignUpForm.subscribe(
-      display => {
-
-        if (display) {
-          this.registerModalRef = this.modal.show(RegisterComponent, this.modalOptions);
-
-          this.registerModalRef.content.displaySignIn = () => {
-            this.closeRegisterModal();
-            this.modalService.displaySignInForm(true);
-          };
-
-          this.registerModalRef.content.closeModal.subscribe(
-            close => {
-              if (close) {
-                this.closeRegisterModal();
-              }
-            }
-          );
-        }
-        else {
-          this.closeRegisterModal();
-        }
-
-      }
-    );
-
-    this.modalService._displaySignInForm.subscribe(
-      display => {
-        if (display) {
-
-          this.signInModalRef = this.modal.show(LoginComponent, this.modalOptions);
-
-          // hide signin form
-          this.signInModalRef.content.displaySignup = () => {
-            this.closeSignInModal();
-            this.modalService.displaySignUpForm(true);
-          };
-
-          this.signInModalRef.content.closeModal.subscribe(
-            close => {
-              if (close) {
-                this.closeSignInModal();
-              }
-            }
-          );
-        }
-        else {
-          this.closeSignInModal();
-        }
-      }
-    );
-  }
-
-
-  closeSignInModal() {
-    if (this.signInModalRef) {
-      this.signInModalRef.hide();
-    }
-  }
-
-  closeRegisterModal() {
-    if (this.registerModalRef) {
-      this.registerModalRef.hide();
-    }
+  constructor() {
   }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AppURLRepo } from '../../utils/app-url-repo';
 import { ModalService } from '../services/modal-service.service';
 import { Router, NavigationEnd } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-header',
@@ -19,13 +22,21 @@ export class HeaderComponent implements OnInit {
   public loggedInUser: boolean;
   private isHomeRouteActivated: boolean;
 
-  constructor(private modalService: ModalService,
-              private router: Router) {
+  private registerModalRef: BsModalRef;
+  private signInModalRef: BsModalRef;
+  private modalOptions: any;
+
+  constructor(private router: Router,
+              private modalService: BsModalService) {
     this.notificationManagerLogo = AppURLRepo.NOTIFICATIONS_MANAGER_LOGO;
     this.profileIcon = AppURLRepo.PROFILE_ICON;
     this.dropdownIcon = AppURLRepo.DROPDOWN_ICON;
     this.settingIcon = AppURLRepo.SETTINGS_ICON;
     this.signoutIcon = AppURLRepo.SIGNOUT_ICON;
+
+    this.modalOptions = {
+      ignoreBackdropClick: true,
+    };
   }
 
   ngOnInit() {
@@ -46,20 +57,52 @@ export class HeaderComponent implements OnInit {
   }
 
   displaySignInForm() {
-    this.modalService.displaySignInForm(true);
+    // this.modalService.displaySignInForm(true);
+    this.signInModalRef = this.modalService.show(LoginComponent, this.modalOptions);
+
+    this.signInModalRef.content.displaySignup = () => {
+      this.closeSignInModal();
+      this.displaySignUpForm();
+    };
+
+    this.signInModalRef.content.closeModal.subscribe(
+      close => {
+        if (close) {
+          this.closeSignInModal();
+        }
+      }
+    );
   }
 
   displaySignUpForm() {
-    this.modalService.displaySignUpForm(true);
+    // this.modalService.displaySignUpForm(true);
+    this.registerModalRef = this.modalService.show(RegisterComponent, this.modalOptions);
+
+    this.registerModalRef.content.displaySignIn = () => {
+      this.closeRegisterModal();
+      this.displaySignInForm();
+    };
+
+    this.registerModalRef.content.closeModal.subscribe(
+      close => {
+        if (close) {
+          this.closeRegisterModal();
+        }
+      }
+    );
   }
 
-  // hideSignInForm() {
-  //   this.modalService.displaySignInForm(false);
-  // }
-  //
-  // hideSignUpForm() {
-  //   this.modalService.displaySignUpForm(false);
-  // }
+  closeSignInModal() {
+    if (this.signInModalRef) {
+      this.signInModalRef.hide();
+    }
+  }
+
+  closeRegisterModal() {
+    if (this.registerModalRef) {
+      this.registerModalRef.hide();
+    }
+  }
 
   processLogout() {
 
